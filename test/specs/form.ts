7ed386ea7da8d5118987d24c25676c97a9d597a8
Joacim Magnusson <joacim.magnusson@seb.se>
2025-09-12 16:59:52 +0200

    Update form test

--- test/specs/form.ts
+++ test/specs/form.ts
@@ -10,12 +10,12 @@ describe("Form /", () => {
     expect(form).toBeDisplayed();
   });
 
-  //   it("should tab in the correct sequence", async () => {
-  //     for (let i = 1; i <= 21; i++) {
-  //       await browser.keys("Tab");
-  //       await expect(browser).toMatchScreenSnapshot(`form-tab${i}`);
-  //     }
-  //   });
+  // it("should tab in the correct sequence", async () => {
+  //   for (let i = 1; i <= 21; i++) {
+  //     await browser.keys("Tab");
+  //     await expect(browser).toMatchScreenSnapshot(`form-tab${i}`);
+  //   }
+  // });
 
   it("should not show any errors initially", async () => {
     const form = $("form");
@@ -25,17 +25,24 @@ describe("Form /", () => {
 
   it("should show errors when submitting empty fields", async () => {
     const submitButton = $("#submit-button");
-    await submitButton.click();
+    await submitButton.execute((el) => {
+      el.shadowRoot?.querySelector("button")?.click();
+    });
+
     await browser.pause(500); // Wait for validation to complete
 
     const formElements = await $$(".form-control");
 
     for (const element of formElements) {
-      const errorMessage = element.$(".error-message slot");
-      //console.log("text: ", await errorMessage.getText());
-      expect(await errorMessage.getText()).toContain(
-        "This field cannot be empty"
-      );
+      const isInvalid = (await element.execute(
+        (el: any) => el.invalid
+      )) as boolean;
+      const errorMessage = (await element.execute(
+        (el: any) => el.errorMessage
+      )) as string;
+
+      expect(isInvalid).toBe(true);
+      expect(errorMessage).toContain("This field cannot be empty");
     }
   });
 });
